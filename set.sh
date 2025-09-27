87089631a1a74e9752aa3a24da166a39d33905b0
alexDoron073 <163978982+alexDoron073@users.noreply.github.com>
2025-09-27 09:55:25 +0300

    Update set.sh

--- set.sh
+++ set.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-echo -e $SUDO_USER
-# echo -e "$SUDO_USER\tALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
-# apt install neofetch
+cd ~
 
-# echo -e "sudo -i\nexit" >> .bashrc
-# wget https://raw.githubusercontent.com/alexDoron073/setsh/refs/heads/main/.bashrc -P $HOME/
-# source .bashrc
+echo -e "$SUDO_USER\tALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
+apt install neofetch
+
+echo -e "sudo -i\nexit" >> .bashrc
+wget https://raw.githubusercontent.com/alexDoron073/setsh/refs/heads/main/.bashrc -P $HOME/
+source .bashrc
